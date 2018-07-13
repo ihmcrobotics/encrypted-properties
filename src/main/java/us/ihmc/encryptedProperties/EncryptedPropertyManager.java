91a2b7db3427a1491df1432dfbda031ce1b3be5e
Duncan Calvert <dcalvert@ihmc.us>
2018-07-12 22:00:59 -0500

    Add method to specify passphrase and property location.

--- src/main/java/us/ihmc/encryptedProperties/EncryptedPropertyManager.java
+++ src/main/java/us/ihmc/encryptedProperties/EncryptedPropertyManager.java
@@ -3,6 +3,7 @@ package us.ihmc.encryptedProperties;
 import nu.studer.java.util.OrderedProperties;
 
 import java.io.File;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 
@@ -11,16 +12,22 @@ public class EncryptedPropertyManager
    // from https://github.com/etiennestuder/gradle-credentials-plugin/blob/master/src/main/groovy/nu/studer/gradle/credentials/CredentialsPlugin.java
    public static final String DEFAULT_PASSPHRASE = ">>Default passphrase to encrypt passwords!<<";
 
-   public static HashMap<String, String> loadEncryptedCredentials()
+   public static HashMap<String, String> loadEncryptedCredentials(Path enryptedPropertiesFile, String passphrase)
    {
-      CredentialsEncryptor credentialsEncryptor = CredentialsEncryptor.withPassphrase(DEFAULT_PASSPHRASE.toCharArray());
-      File credentialsFile = Paths.get(System.getProperty("user.home"), ".gradle", "gradle.encrypted.properties").toFile();
+      CredentialsEncryptor credentialsEncryptor = CredentialsEncryptor.withPassphrase(passphrase.toCharArray());
+      File credentialsFile = enryptedPropertiesFile.toFile();
       CredentialsPersistenceManager credentialsPersistenceManager = new CredentialsPersistenceManager(credentialsFile);
       HashMap decryptedProperties = new HashMap<String, String>();
       OrderedProperties credentials = credentialsPersistenceManager.readCredentials();
       for (String name : credentials.stringPropertyNames())
       {
          decryptedProperties.put(name, credentialsEncryptor.decrypt(credentials.getProperty(name)));
-      } return decryptedProperties;
+      }
+      return decryptedProperties;
+   }
+
+   public static HashMap<String, String> loadEncryptedCredentials()
+   {
+      return loadEncryptedCredentials(Paths.get(System.getProperty("user.home"), ".gradle", "gradle.encrypted.properties"), DEFAULT_PASSPHRASE);
    }
 }
